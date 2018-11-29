3bccf0f55aa60037ad680ecc6c64621a8a6d7c1c
Michal Valousek <michal@plovarna.cz>
2018-11-29 22:30:45 +0100

    Updated run.sh BASH script, fullpath to python sciprt

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 setterm -foreground black
 clear
-python3 ./tapestry.py $1 &>> /var/log/tapestry.log
+python3 $DIR/tapestry.py $1 &> /var/log/tapestry.log
 setterm -foreground white
 clear
